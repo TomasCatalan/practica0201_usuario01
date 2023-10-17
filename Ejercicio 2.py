d5de4e07d4d83db2bf396767e1b17ebc59754dc7
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-10-17 12:48:09 +0200

    Update Ejercicio 2.py

---
+++ Ejercicio 2.py
@@ -0,0 +1,9 @@
+x = input("introduce tu nombre completo")
+
+print("Nombre completo en minusculas ", x.lower())
+print("Nombre completo en mayusculas ", x.upper())
+
+(y) = x.title()
+
+print("Nombre completo con las iniciaeles en mayusculas", y)
+
