73b01608abb5342d58048f324ed26e8a1627feb5
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-10-20 11:28:35 +0200

    Update Ejercicio 11.py

---
+++ Ejercicio 11.py
@@ -0,0 +1,9 @@
+x = input("Introduzca el nombre del producto")
+y = float(input("Introduzca el precio unitario"))
+z = float(input("Introduzca el numero de unidades"))
+
+a = round(y, 2)
+b = round((a * z), 2)
+
+print(a)
+print(b)
